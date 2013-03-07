34a6fc5298ec6bb1908fe823cc486c17030dcec8
batatero <ale.ribas@hotmail.com>
2013-03-07 17:13:18 -0300

    adicionado class business

--- application/libraries/Dao/DaoInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-interface DaoInterface {
-	public function insert();
-	public function update();
-	public function delete();
-	public function findAll();
-	public function findById();
-}
---
+++ application/libraries/business/factoryBusiness.php
@@ -0,0 +1,23 @@
+<?php
+include ('application/models/DAO/genericDAO.php');
+
+
+class FactoryBusiness {
+	private static $instance = null;
+	
+	public static function getInstance( $objName ) {
+		include ('application/models/DAO/'.$objName.'.php');
+		//verifica se ja existe uma stancia de um DAO
+		if ( (self::$instance == null) OR !($objName instanceof $objName) ) { 
+			self::$instance = new $objName();
+		}
+		
+		return self::$instance;
+	}
+	
+	//instancia da model e do core do code CI
+	protected function getCI () {
+		$CI = &get_instance();
+		return $CI;
+	}
+}
